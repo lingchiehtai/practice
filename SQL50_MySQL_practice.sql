0048d3db66e1d46e5e3e14cce5176efe0dc007cf
lingchiehtai <lindatai0305@gmail.com>
2024-08-29 16:16:02 +0800

    Update SQL50_MySQL_practice.sql

--- SQL50_MySQL_practice.sql
+++ SQL50_MySQL_practice.sql
@@ -7,3 +7,23 @@ select product_id
 from Products 
 where low_fats='Y' and recyclable='Y'
 
+#584. Find Customer Referee
+select name 
+from Customer 
+where referee_id!=2 or referee_id is null
+
+#595. Big Countries
+select name,  population, area 
+from World 
+where area>=3000000 or population>=25000000
+
+#1148. Article Views I
+SELECT DISTINCT author_id as id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id;
+
+#1683. Invalid Tweets
+select tweet_id 
+from Tweets 
+where length(content)>15
