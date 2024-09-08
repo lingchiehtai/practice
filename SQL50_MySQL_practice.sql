27c79bbb94381a38512cc46546faa37898ca4e73
lingchiehtai <lindatai0305@gmail.com>
2024-09-08 18:03:51 +0800

    Update SQL50_MySQL_practice.sql

--- SQL50_MySQL_practice.sql
+++ SQL50_MySQL_practice.sql
@@ -77,4 +77,10 @@ from Cinema
 where id % 2 !=0 and description !='boring'
 order by rating desc
 
+#1251. Average Selling Price
+select p.product_id, ifnull(round(sum(p.price*u.units)/sum(u.units),2),0) as average_price
+from Prices p left join UnitsSold u 
+on p.product_id=u.product_id and (u.purchase_date between p.start_date and p.end_date)
+group by p.product_id
+
 
