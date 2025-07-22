758213bce3ae03d6c80ab4259e7017ad67629e30
lingchiehtai <lindatai0305@gmail.com>
2025-07-22 10:27:10 +0800

    update

---
+++ practice2/hotel-price-1month_plot.py
@@ -0,0 +1,108 @@
+"""
+@author: Linda Tai
+"""
+
+#plot
+import matplotlib.pyplot as plt
+import csv
+
+
+
+with open('hotel-price-1month_h1_mitsui.csv', newline='') as csvfile:
+    rows = csv.reader(csvfile)   
+    x1 = []
+    y1 = []
+    for row in rows:
+        x1.append(row[0][5:]) #12/31 (year not shown)
+        y1.append(int(row[2])) 
+
+with open('hotel-price-1month_h2_intergate.csv', newline='') as csvfile2:
+    rows = csv.reader(csvfile2)   
+    y2 = []
+    for row in rows:
+        y2.append(int(row[2]))
+        
+with open('hotel-price-1month_h3_daiwa-roynet.csv', newline='') as csvfile3:
+    rows = csv.reader(csvfile3)   
+    y3 = []
+    for row in rows:
+        y3.append(int(row[2]))
+        
+with open('hotel-price-1month_h4_nohga.csv', newline='') as csvfile4:
+    rows = csv.reader(csvfile4)   
+    y4 = []
+    for row in rows:
+        y4.append(int(row[2]))
+       
+
+with open('hotel-price-1month_h5_cross.csv', newline='') as csvfile5:
+    rows = csv.reader(csvfile5)   
+    y5 = []
+    for row in rows:
+        y5.append(int(row[2])) 
+
+with open('hotel-price-1month_h6_m-39-s.csv', newline='') as csvfile6:
+    rows = csv.reader(csvfile6)   
+    y6 = []
+    for row in rows:
+        y6.append(int(row[2]))
+        
+with open('hotel-price-1month_h7_fresa-inn.csv', newline='') as csvfile7:
+    rows = csv.reader(csvfile7)   
+    y7 = []
+    for row in rows:
+        y7.append(int(row[2]))
+        
+with open('hotel-price-1month_h8_prince-smart.csv', newline='') as csvfile8:
+    rows = csv.reader(csvfile8)   
+    y8 = []
+    for row in rows:
+        y8.append(int(row[2]))
+
+
+fig1 = plt.figure(figsize=(10,5), dpi=120)  #Default=>dpi=80
+
+
+plt.plot(x1, y3, color = 'y', marker='o', linestyle = '--', label='h3')
+plt.plot(x1, y2, color = 'yellowgreen', marker='D', linestyle = '--', label='h2')
+plt.plot(x1, y1, color = 'red', marker='o', linestyle = '--', label='h1')
+
+plt.legend(loc = 'upper right')
+plt.xlabel('Date', size=20) #, color = 'blue')
+plt.ylabel('Price (yen)', size=20) #, color = 'blue')
+
+plt.title('Daily Room Rate', size=18) #, color = 'blue')
+
+stride = 7 #每n個刻度顯示一次標籤",
+plt.xticks(ticks=x1[1:62:stride], labels=x1[1:62:stride], fontsize=16, rotation=0)
+plt.ylim(5000, 85000)
+labely= ['10k','20k','30k','40k','50k','60k','70k','80k']
+plt.yticks([10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000], labels=labely, fontsize=16)
+
+plt.grid(
+    visible=True,  #是否顯示網格，預設False不顯示，設定True則顯示。
+    axis='x',
+    #color='#c00',
+    dashes=(2,2),
+    linewidth=2,
+    alpha=0.5) #隔線透明度，預設 1 完全不透明，設定 0 完全透明。
+
+
+plt.savefig('hotel-price-1month_1.png') 
+
+
+#----------------------------------------------
+#合理價出現機率
+
+def countReasonablePrice(y_price):
+    #y_price_min = min(y_price)
+    y_price_len = len(y_price)
+    
+    #合理價 = 最低價的1.3倍
+    y_price_index = [i for i in range(len(y_price)) if y_price[i] < min(y_price)*1.3 ] 
+    y_price_low_count = len(y_price_index) #合理價次數
+    print(f'{y_price_low_count}, {y_price_len}, {100*y_price_low_count/y_price_len:.1f}%') #合理價出現機率
+    
+countReasonablePrice(y1)
+countReasonablePrice(y2)
+countReasonablePrice(y3)
---
+++ practice2/hotel-price-1month_v2.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Linda
+網路爬蟲 訂房網站
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+import time
+
+
+###############################
+def GetRoomPrice_EachMonth(hotelname, hotelWebsite, roomTypeBlockID, year, mon1):
+           
+    headers = {
+        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        "Cookie" : "pcm_consent=analytical%3Dtrue%26countryCode%3DTW%26consentId%3D875e90ea-7348-4330-b5d1-03faddf8187a%26consentedAt%3D2024-03-12T06%3A20%3A03.629Z%26expiresAt%3D2024-09-08T06%3A20%3A03.629Z%26implicit%3Dtrue%26marketing%3Dtrue%26regionCode%3DKHH%26regulation%3Dnone%26legacyRegulation%3Dnone; px_init=0; pcm_personalization_disabled=0; bkng_sso_auth=CAIQsOnuTRpm6tVsG2KF9N49yWQYBp24ROpDXRcMoo7jp4HIJn4mQVdrashUBtSj3dX3CCKUr04n8fJmNdkT127QeTgyuV1i0AHm1UJYW1K6pbV4p8oYKExHP1hN0rSqYn3dnGpHha2uRkFY4Mwz; cors_js=1; aws-waf-token=44fae8e6-45ef-4b17-ba42-345794e1f574:AAoAuzYku6fmAAAA:QHuaUufa9qPHmPHdGS2ZohGK34xjJyhfFTWBgYlhynR2gGi5KhsNKShr/k93bQ08hLb2knWXrR5+dAHuJjOmb+8N4XW0RMZ7hhne7ZFaxuaHJsaWyn5wxkQXJHLWSxSNCnmiLZcsHo5z96MXvAW/DvdTweHupHSI5sgIiChfE2zoUjBOW33E/m6fyT8RFYSU03ehOCOm5D+GAkUoZ/TuwTC5j6FyJt2cfOAC7UO6Z6MZtoTas+7Jio1nklJLNcZ7mD4c; bkng=11UmFuZG9tSVYkc2RlIyh9YSvtNSM2ADX0BnR0tqAEmjtAzvsn8OY9T4VXEHFF8cK9IuYdVsrhjpXynuy%2BITrWXvd169hYatAr5KnCQBtj6PXAuvNa6k02m7cMa%2Ff0AIcktOZS4yOj3To4hlXMYYwmM%2Bc%2B9Eh4jif%2BvucA1v%2BliBQJw2wf9yf4qazwETH2xwrQmq3RAabxuJHLsxc6xg%2BLgA%3D%3D; lastSeen=1712726850435"
+    }
+        
+    dict1 = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31} #每個月有幾天
+    StartDate = 1
+    EndDate = dict1[mon1]
+    
+    y1 = year 
+    y2 = y1
+    dataPriceInfo = []
+    for date1 in range(StartDate, EndDate+1):
+        if date1 == dict1[mon1]:
+            date2 = 1
+            if mon1 ==12:
+                mon2 = 1
+                y2 = y1 + 1
+            else:
+                mon2 = mon1+1
+            
+        else:
+            mon2 = mon1
+            date2 = date1+1
+        
+        #hotel info
+        bookURL = hotelWebsite + f'checkin={y1}-{mon1:02d}-{date1:02d}&checkout={y2}-{mon2:02d}-{date2:02d}&do_availability_check=1&group_adults=2&group_children=0&selected_currency=hotel_currency'           
+    
+        try:
+    
+            res = requests.get(bookURL, headers=headers)        
+            soup=BeautifulSoup(res.text,'html.parser')  
+            #print(res.status_code)
+            
+            table = soup.find('tr', {'data-block-id': roomTypeBlockID}) #鎖定某一房型    
+            RoomType = table.find('span', class_='hprt-roomtype-icon-link').text.strip()
+            
+            text1 = table.find('span', class_='prco-valign-middle-helper').text.strip()
+            RoomPrice = text1[1:((len(text1)-4))] + text1[-3:] #不包含幣值的符號 ¥
+            time.sleep(0.1)
+        except:
+            RoomType = 'None'
+            RoomPrice = '0'
+               
+        
+        date = f'{year}/{mon1:02d}/{date1:02d}'
+        print(date)     
+                    
+        dataPriceInfo.append([date, RoomType, RoomPrice])
+    
+    #write to csv file
+    csvFilename = f'hotel-price-1month_{hotelname}.csv'
+    with open(csvFilename, 'a', newline="", encoding='UTF-8') as file1:
+        writer = csv.writer(file1)
+        writer.writerows(dataPriceInfo)
+
+    
+
+
+###  輸入參數 ##################
+#以取得不同飯店/不同日期的房價
+hotelname = 'Yokohama_Tokyu_REI'
+hotelWebsite = 'https://www.booking.com/hotel/jp/heng-bang-dong-ji-reihoteru.zh-tw.html?' 
+dataBlockID = '798044002_376701712_0_2_0'
+
+#dataPriceInfo = GetRoomPrice_EachMonth(hotelname, hotelWebsite, dataBlockID, 2024, 10)
+dataPriceInfo = GetRoomPrice_EachMonth(hotelname, hotelWebsite, dataBlockID, 2024, 11)
+dataPriceInfo = GetRoomPrice_EachMonth(hotelname, hotelWebsite, dataBlockID, 2024, 12)
+dataPriceInfo = GetRoomPrice_EachMonth(hotelname, hotelWebsite, dataBlockID, 2025, 1)
+dataPriceInfo = GetRoomPrice_EachMonth(hotelname, hotelWebsite, dataBlockID, 2025, 2)
+
+
+print(' == Request is Finished! == ')
+
+
+
+
