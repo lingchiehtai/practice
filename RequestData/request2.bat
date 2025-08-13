59d87ad0f79c582ec1f504df79bfbd86735d9767
lingchiehtai <lindatai0305@gmail.com>
2025-08-13 11:57:53 +0800

    update

--- RequestData/request2.bat
+++ RequestData/request2.bat
@@ -1,4 +1,4 @@
-cd /d C:\Github\practice\RequestData
+cd /d C:\Users\user\Documents\GitHub\practice\RequestData
 
-call C:\Users\Linda\anaconda3\envs\env1\python Request_cwa_weather.py
+call C:\Users\user\anaconda3\envs\env1\python Request_cwa_weather.py
 
